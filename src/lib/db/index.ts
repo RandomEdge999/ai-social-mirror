422245545aeb7515777f85cdaa988905a0a7f6d0
Aleem Azhar <114960296+RandomEdge999@users.noreply.github.com>
2025-07-29 10:35:25 -0700

    fix(db): clean export line

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -8,4 +8,4 @@ const connectionString = process.env.DATABASE_URL!
 const client = postgres(connectionString, { prepare: false })
 export const db = drizzle(client, { schema })
 
-export * from './schema' 
+export * from './schema'
